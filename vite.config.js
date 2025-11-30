ceade705cc9fb4f93e6db15e5d4f371d344f622e
rohanradeeh <rohanradeeh@gmail.com>
2025-11-30 15:47:53 +0530

    Fix blank page by adding base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This line is crucial! It tells the app it lives in this subfolder.
+  base: "/my-portfolio/", 
 })
